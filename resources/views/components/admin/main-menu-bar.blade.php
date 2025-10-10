7d96512b589290e266aac8eb813d3025adbd0b75
Deocomate <deocomate@gmail.com>
2025-10-10 11:14:33 +0700

    Update UI

--- resources/views/components/admin/main-menu-bar.blade.php
+++ resources/views/components/admin/main-menu-bar.blade.php
@@ -10,19 +10,19 @@
             :route="route('admin.dashboard.index')"
             name="Tổng quan"
             icon="fas fa-tachometer-alt"
-            :route-group="['admin.dashboard.*']" />
+            :route-group="['admin.dashboard.*']"/>
 
         <x-menus.menu-bar
             :route="route('admin.bookings.index')"
             name="Quản lý Đặt vé"
             icon="fas fa-ticket-alt"
-            :route-group="['admin.bookings.*']" />
+            :route-group="['admin.bookings.*']"/>
 
         <x-menus.menu-bar
             :route="route('admin.companies.index')"
             name="Quản lý Nhà xe"
             icon="fas fa-building"
-            :route-group="['admin.companies.*']" />
+            :route-group="['admin.companies.*']"/>
 
         <li class="nav-header">QUẢN LÝ DANH MỤC</li>
 
@@ -30,23 +30,23 @@
             icon="fas fa-map-marked-alt"
             name="Quản lý Địa điểm"
             :route-group="['admin.provinces.*', 'admin.districts.*', 'admin.district-types.*', 'admin.stops.*']">
-            <x-menus.menu-item name="Tỉnh/Thành phố" :route="route('admin.provinces.index')" />
-            <x-menus.menu-item name="Quận/Huyện" :route="route('admin.districts.index')" />
-            <x-menus.menu-item name="Loại địa điểm" :route="route('admin.district-types.index')" />
-            <x-menus.menu-item name="Điểm dừng" :route="route('admin.stops.index')" />
+            <x-menus.menu-item name="Tỉnh/Thành phố" :route="route('admin.provinces.index')"/>
+            <x-menus.menu-item name="Quận/Huyện" :route="route('admin.districts.index')"/>
+            <x-menus.menu-item name="Loại địa điểm" :route="route('admin.district-types.index')"/>
+            <x-menus.menu-item name="Điểm dừng" :route="route('admin.stops.index')"/>
         </x-menus.menu-bar>
 
         <x-menus.menu-bar
             :route="route('admin.bus-services.index')"
             name="Dịch vụ Xe"
             icon="fas fa-concierge-bell"
-            :route-group="['admin.bus-services.*']" />
+            :route-group="['admin.bus-services.*']"/>
 
         <x-menus.menu-bar
             :route="route('admin.routes.index')"
             name="Quản lý Tuyến đường"
             icon="fas fa-route"
-            :route-group="['admin.routes.*']" />
+            :route-group="['admin.routes.*']"/>
 
         <li class="nav-header">HỆ THỐNG</li>
 
@@ -54,8 +54,8 @@
             icon="fas fa-cogs"
             name="Giao diện & Cài đặt"
             :route-group="['admin.menus.*', 'admin.web_profiles.*']">
-            <x-menus.menu-item name="Quản lý Menu" :route="route('admin.menus.index')" />
-            <x-menus.menu-item name="Thông tin Website" :route="route('admin.web_profiles.index')" />
+            <x-menus.menu-item name="Quản lý Menu" :route="route('admin.menus.index')"/>
+            <x-menus.menu-item name="Thông tin Website" :route="route('admin.web_profiles.index')"/>
         </x-menus.menu-bar>
 
     @elseif($userRole === 'company')
@@ -68,28 +68,28 @@
             :route="route('company.dashboard.index')"
             name="Tổng quan"
             icon="fas fa-tachometer-alt"
-            :route-group="['company.dashboard.*']" />
+            :route-group="['company.dashboard.*']"/>
 
         <x-menus.menu-bar
             :route="route('company.bookings.index')"
             name="Quản lý Đặt vé"
             icon="fas fa-ticket-alt"
-            :route-group="['company.bookings.*']" />
+            :route-group="['company.bookings.*']"/>
 
         <x-menus.menu-bar
             icon="fas fa-bus"
             name="Quản lý Vận hành"
             :route-group="['company.buses.*', 'company.routes.*', 'company.bus_routes.*']">
-            <x-menus.menu-item name="Danh sách xe" :route="route('company.buses.index')" />
-            <x-menus.menu-item name="Tuyến đường của nhà xe" :route="route('company.company-routes.index')" />
-            <x-menus.menu-item name="Quản lý Chuyến xe" :route="route('company.bus-routes.index')" />
+            <x-menus.menu-item name="Danh sách xe" :route="route('company.buses.index')"/>
+            <x-menus.menu-item name="Tuyến đường của nhà xe" :route="route('company.company-routes.index')"/>
+            <x-menus.menu-item name="Quản lý Chuyến xe" :route="route('company.bus-routes.index')"/>
         </x-menus.menu-bar>
 
         <x-menus.menu-bar
             :route="route('company.profile.index')"
             name="Thông tin Nhà xe"
             icon="fas fa-id-card"
-            :route-group="['company.profile.*']" />
+            :route-group="['company.profile.*']"/>
 
     @endif
 </ul>
